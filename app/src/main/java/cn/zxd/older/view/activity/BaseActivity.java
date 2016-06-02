3109005a305571244743a0efc3399d11e5cb9d81
danielZhang0601 <daniel.zhang@wolaidai.com>
2016-06-02 19:46:50 +0800

    MainActivity

--- app/src/main/java/cn/zxd/older/view/activity/BaseActivity.java
+++ app/src/main/java/cn/zxd/older/view/activity/BaseActivity.java
@@ -11,9 +11,5 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //透明状态栏
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-        }
     }
 }
--- app/src/main/java/cn/zxd/older/view/activity/LauncherActivity.java
+++ app/src/main/java/cn/zxd/older/view/activity/LauncherActivity.java
@@ -1,9 +1,11 @@
 package cn.zxd.older.view.activity;
 
 import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.view.WindowManager;
 
 import com.badoo.mobile.util.WeakHandler;
 
@@ -12,6 +14,10 @@ public class LauncherActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //透明状态栏
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        }
         View contentView = new View(this);
         setContentView(contentView);
         contentView.setBackgroundColor(Color.WHITE);
@@ -25,6 +31,6 @@ public class LauncherActivity extends BaseActivity {
     }
 
     private void toNext() {
-        LoginActivity.launch(this);
+        MainActivity.launch(this);
     }
 }

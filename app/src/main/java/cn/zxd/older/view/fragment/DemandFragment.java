f0f97aa60ae4d8d424dbcfc4da1eece24a666593
danielZhang0601 <daniel.zhang@wolaidai.com>
2016-06-12 11:35:48 +0800

    change to Amap

--- app/src/main/java/cn/zxd/older/view/fragment/DemandFragment.java
+++ app/src/main/java/cn/zxd/older/view/fragment/DemandFragment.java
@@ -8,14 +8,23 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.zxd.older.R;
+
+
 /**
  */
 
 public class DemandFragment extends Fragment {
 
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return super.onCreateView(inflater, container, savedInstanceState);
+        View fragmentView = inflater.inflate(R.layout.fragement_demand, container);
+        ButterKnife.bind(this, fragmentView);
+        return fragmentView;
     }
 }

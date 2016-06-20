42a4162c9a1d4c3f0f81bf263f0f89cce8c43a9b
danielZhang0601 <daniel.zhang@wolaidai.com>
2016-06-21 02:09:59 +0800

    doing sign up

---
+++ app/src/main/java/cn/zxd/older/view/activity/GetPasswordActivity.java
@@ -0,0 +1,18 @@
+package cn.zxd.older.view.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import cn.zxd.older.R;
+
+/**
+ * Created by Daniel on 2016/6/21.
+ */
+public class GetPasswordActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up_or_get_password);
+    }
+}
